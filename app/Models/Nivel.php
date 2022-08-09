037f951a218056e792f5f86feb2b58fb1747b39d
Ivan Castro <ivan20cast@gmail.com>
2022-08-09 15:42:45 -0500

    Migracion/Modelo tabla niveles

---
+++ app/Models/Nivel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Nivel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'niveles';
+
+    protected $fillable = [
+        'valor',
+        'nombre',
+    ];
+}

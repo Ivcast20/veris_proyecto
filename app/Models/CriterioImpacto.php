1f6080989e894ee3c44f6a986e01402b5b0255a7
Ivan Castro <ivan20cast@gmail.com>
2022-08-09 16:15:16 -0500

    Migracion/Modelo tabla criterio_impactos

---
+++ app/Models/CriterioImpacto.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CriterioImpacto extends Model
+{
+    use HasFactory;
+
+    protected $table = 'criterio_impactos';
+}
